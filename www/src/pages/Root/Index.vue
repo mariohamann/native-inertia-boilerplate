66143ea94566101112be514551efa7bf4150f073
Mario Hamann <mariovhamann@gmail.com>
2023-06-07 23:12:34 +0200

    perf: make loop and improve speed

--- www/src/pages/Root/Index.vue
+++ www/src/pages/Root/Index.vue
@@ -1,24 +1,40 @@
 <script setup lang="ts">
-import {Link} from "@inertiajs/vue3";
+import { router } from "@inertiajs/vue3";
 
 let props = defineProps<{
-  time: string;
+	time: string;
 }>();
+
+const handleClick = () => {
+	const start = Date.now().toString();
+	window["start"] = start;
+	router.get("/");
+	console.count("fetch" + start);
+};
+
+if (window.localStorage.getItem("start")) {
+	const start = parseInt(window["start"]);
+	const now = Date.now();
+	const diff = now - start;
+	if (diff < 10000) {
+		router.get("/");
+		console.count("fetch" + start);
+	}
+}
 </script>
 
 <template>
-  <header>
-  </header>
+	<header></header>
 
-  <main>
-    <h1>Loaded: {{ props.time }}</h1>
-    <Link v-for="i in 50" :key="i" :href="`/${i}`">{{ i }}</Link>
-  </main>
+	<main>
+		<h1>Loaded: {{ props.time }}</h1>
+		<button @click="handleClick">Loop it!</button>
+	</main>
 </template>
 
 <style scoped>
-  a{
-    display: block;
-    margin-bottom: 0.25rem;
-  }
+a {
+	display: block;
+	margin-bottom: 0.25rem;
+}
 </style>
